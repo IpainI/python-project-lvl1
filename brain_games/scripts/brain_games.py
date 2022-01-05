dd0b363fcea7ad87c47a777d724e09e1f9d91594
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-05 16:27:56 +0300

    Add files via upload

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.cly import user_name
+
+
+def main() -> object:
+    print('Welcome to the Brain Games!')
+    name = user_name()
+
+
+if __name__ == '__main__':
+    main()
