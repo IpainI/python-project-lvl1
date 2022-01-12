b06003dbddbf79781eb526be96180963540edef9
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-12 14:04:11 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,8 +6,8 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 def get_question_and_result():
 
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
+    number1 = random.randint(1, 50)
+    number2 = random.randint(1, 50)
     result = str(math.gcd(number1, number2))
     expression = 'Question: {} {} '.format(number1, number2)
     return expression, result
