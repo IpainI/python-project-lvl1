f2901a0eace1b0327b9eb447fbfd56cd757ec18e
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-12 14:03:05 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,8 +7,8 @@ ALLOWED_OPERATIONS = ['+', '-', '*']
 def get_question_and_result():
 
     operation = random.choice(ALLOWED_OPERATIONS)
-    number1 = random.randint(1, 40)
-    number2 = random.randint(1, 40)
+    number1 = random.randint(1, 20)
+    number2 = random.randint(1, 20)
     if operation == '+':
         result = str(number1 + number2)
     elif operation == '-':
