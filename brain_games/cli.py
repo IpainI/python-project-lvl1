2f2356cc53e33d72b9aa1052df4d136f8f5edc5a
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:52:18 +0300

    Update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
 
 
 def ask_user_name():
-	name = prompt.string('May I have your name? ')
-	print('Hello, {}!'.format(name))
-	return name
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
+    return name
