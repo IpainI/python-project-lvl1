1973cceaa155d975f25c9cd5b0bc480b19b9d48c
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:58:28 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from brain_games.games import gcd
-from brain_games.engine import game_process
+from brain_games.engine import start_game_process
 
 
 def main():
     print('Welcome to the Brain Games!')
-    game_process(gcd)
+    start_game_process(gcd)
 
 
 if __name__ == '__main__':
