5cb5221a43039b440a8605ed83786671ee0e198d
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:59:06 +0300

    Update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from brain_games.games import progression
-from brain_games.engine import game_process
+from brain_games.engine import start_game_process
 
 
 def main():
     print('Welcome to the Brain Games!')
-    game_process(progression)
+    start_game_process(progression)
 
 
 if __name__ == '__main__':
