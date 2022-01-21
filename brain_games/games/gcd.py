53cf5a835d61e2cfa8b60147a49714d964263dc5
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:46:54 +0300

    Update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,5 +9,5 @@ def get_question_and_result():
     number1 = random.randint(1, 50)
     number2 = random.randint(1, 50)
     result = str(math.gcd(number1, number2))
-    expression = 'Question: {} {} '.format(number1, number2)
+    expression = '{} {} '.format(number1, number2)
     return expression, result
