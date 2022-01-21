6f6602d400faccb3777ed698de23377246623319
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:47:47 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,19 +4,16 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    count = 0
-    if number < 0:
-        return 'no'
-    for i in range(1, number // 2 + 1):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
-            count += 1
-            if count > 1 or number == 1:
-                return 'no'
-    return 'yes'
+            return False
+    return True
 
 
 def get_question_and_result():
     number = random.randint(1, 50)
-    result = is_prime(number)
-    expression = 'Question: {}'.format(number)
+    result = 'yes' if is_prime(number) else 'no'
+    expression = '{}'.format(number)
     return expression, result
