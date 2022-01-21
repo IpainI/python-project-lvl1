794e8fed80005fd6c8e301b368dfa72d62f4c192
IpainI <90567777+IpainI@users.noreply.github.com>
2022-01-21 12:58:01 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from brain_games.games import even
-from brain_games.engine import game_process
+from brain_games.engine import start_game_process
 
 
 def main():
     print('Welcome to the Brain Games!')
-    game_process(even)
+    start_game_process(even)
 
 
 if __name__ == '__main__':
